86a1ab63b400637cffb8b55d8c2f88b849ba5d81
ISHIKAWA Takayuki <topstone@users.noreply.github.com>
2022-03-14 04:59:51 +0900

    update mount dirs

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # trtext-run
- TRTEXT runner with DOSVAXJ3
+
+TRTEXT runner with DOSVAXJ3
+
+TRTEXT を DOSVAXJ3 上で動かす batch です。PowerShell script を書く時間がなかったので、昔ながらの batch で書いています。
+
+GPLv2 に従います。
+
+# 使い方
+
+trtext-run.bat を実行すると、以下の通り mount します。
+
+* C: … trtext-run.bat が存在する directory
+* D: Scoop apps の directory
+* E: TRTEXT.EXE が存在する directory
+
+例えば、事前に `scoop install trtext-eng` として英語辞書を導入済みであれば、以下の手順で利用できます。
+
+```
+d:
+cd trtext-eng
+cd current
+e:trtext d:eng
+```
--- trtext-run.bat
+++ trtext-run.bat
@@ -1,11 +1,7 @@
 @echo off
-rem ********************************************************************
-rem * TRTEXT を DOSVAXJ3 上で動かす batch です。
-rem * PowerShell script を書く時間がなかったので、
-rem * 昔ながらの batch で書いています。
-rem ********************************************************************
 if exist trtext_path1.txt del /Q trtext_path1.txt
 if exist trtext_path2.txt del /Q trtext_path2.txt
+if exist trtext_path3.txt del /Q trtext_path3.txt
 call scoop prefix trtext > trtext_path1.txt
 if "%ERRORLEVEL%"=="0" goto :scoop_exist
 echo Scoop was not found.
@@ -17,12 +13,16 @@ for /f "delims=" %%a in (trtext_path1.txt) do (
   set line=%%a
   echo !line:\current=\current\! >> trtext_path2.txt
 )
+for /f "delims=" %%a in (trtext_path1.txt) do (
+  set line=%%a
+  echo !line:\trtext\current=\! >> trtext_path3.txt
+)
 if exist ..\..\dosvaxj3\current\dosboxj.conf goto :dosboxj_conf_exist
 echo ..\..\dosvaxj3\current\dosboxj.conf was not found.
 goto :end
 
 :dosboxj_conf_exist
-if not exist dosboxj.conf copy ..\..\dosvaxj3\current\dosboxj.conf+mount.txt+trtext_path2.txt dosboxj.conf
+if not exist dosboxj.conf copy ..\..\dosvaxj3\current\dosboxj.conf+mount1.txt+trtext_path3.txt+mount2.txt+trtext_path2.txt dosboxj.conf
 if not exist japanese.lng copy ..\..\dosvaxj3\current\japanese.lng japanese.lng
 if not exist jp.kl copy ..\..\dosvaxj3\current\jp.kl jp.kl
 ..\..\dosvaxj3\current\dosboxj.exe
