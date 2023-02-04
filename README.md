d191603d237d8537ee795ecef72a669212fa0219
ISHIKAWA Takayuki <topstone@users.noreply.github.com>
2023-02-05 01:55:21 +0900

    Update: fixed path to English, French, Deutsch, Bahasa Indonesia

--- README.md
+++ README.md
@@ -12,13 +12,14 @@ trtext-run.bat を実行すると、以下の通り mount します。
 
 * C: … trtext-run.bat が存在する directory
 * D: Scoop apps の directory
-* E: TRTEXT.EXE が存在する directory
+* E: 英語辞書が存在する directory
+* F: フランス語辞書が存在する directory
+* G: ドイツ語辞書が存在する directory
+* I: インドネシア語辞書が存在する directory
+* X: TRTEXT.EXE が存在する directory
 
 例えば、事前に `scoop install trtext-eng` として英語辞書を導入済みであれば、以下の手順で利用できます。
 
 ```
-d:
-cd trtext-eng
-cd current
-e:trtext d:eng
+x:trtext e:eng
 ```
--- trtext-run.bat
+++ trtext-run.bat
@@ -1,28 +1,10 @@
 @echo off
-if exist trtext_path1.txt del /Q trtext_path1.txt
-if exist trtext_path2.txt del /Q trtext_path2.txt
-if exist trtext_path3.txt del /Q trtext_path3.txt
-call scoop prefix trtext > trtext_path1.txt
-if "%ERRORLEVEL%"=="0" goto :scoop_exist
-echo Scoop was not found.
-goto :end
-
-:scoop_exist
-setlocal enabledelayedexpansion
-for /f "delims=" %%a in (trtext_path1.txt) do (
-  set line=%%a
-  echo !line:\current=\current\! >> trtext_path2.txt
-)
-for /f "delims=" %%a in (trtext_path1.txt) do (
-  set line=%%a
-  echo !line:\trtext\current=\! >> trtext_path3.txt
-)
 if exist ..\..\dosvaxj3\current\dosboxj.conf goto :dosboxj_conf_exist
 echo ..\..\dosvaxj3\current\dosboxj.conf was not found.
 goto :end
 
 :dosboxj_conf_exist
-if not exist dosboxj.conf copy ..\..\dosvaxj3\current\dosboxj.conf+mount1.txt+trtext_path3.txt+mount2.txt+trtext_path2.txt dosboxj.conf
+if not exist dosboxj.conf copy ..\..\dosvaxj3\current\dosboxj.conf+mount_list.txt dosboxj.conf
 if not exist japanese.lng copy ..\..\dosvaxj3\current\japanese.lng japanese.lng
 if not exist jp.kl copy ..\..\dosvaxj3\current\jp.kl jp.kl
 ..\..\dosvaxj3\current\dosboxj.exe
